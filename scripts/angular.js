5e52816340ac8fe464f38eaab1f4296639b9f7d5
Douglas Rosa <douglasrosa0110@gmail.com>
2020-01-21 22:00:46 -0300

    saving work

--- scripts/angular.js
+++ scripts/angular.js
@@ -1,9 +1,59 @@
 	var app = angular.module("myApp", []);
 	app.controller("myCtrl", function($scope) {
 	  $scope.elements = [
-	  	{code: "3002", x: "724", y: "192", class: "sold", coords_on_click: "820,240,936,274"},
+	  	/* first block */
+	  	{code: "3002", x: "724", y: "192", class: "", coords_on_click: "820,240,936,274"},
 	  	{code: "2902", x: "724", y: "234", class: "", coords_on_click: "820,286,936,317"},
-	  	{code: "2802", x: "724", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "2802", x: "724", y: "282", class: "", coords_on_click: "820,328,936,357"},
+	  	{code: "2702", x: "724", y: "326", class: "", coords_on_click: "820,370,936,400"},
+	  	{code: "2602", x: "724", y: "370", class: "", coords_on_click: "820,417,936,445"},
+	  	{code: "2502", x: "724", y: "415", class: "", coords_on_click: "820,461,936,489"},
+	  	{code: "2402", x: "724", y: "460", class: "", coords_on_click: "820,506,936,534"},
+	  	{code: "2302", x: "724", y: "506", class: "", coords_on_click: "820,548,936,579"},
+	  	{code: "2202", x: "724", y: "552", class: "", coords_on_click: "820,590,936,620"},
+	  	{code: "2102", x: "724", y: "596", class: "", coords_on_click: "820,634,936,664"},
+	  	{code: "2002", x: "724", y: "640", class: "", coords_on_click: "820,678,936,708"},
+	  	{code: "1902", x: "724", y: "687", class: "", coords_on_click: "820,722,936,752"},
+	  	{code: "1802", x: "724", y: "731", class: "", coords_on_click: "820,766,936,794"},
+
+	  	{code: "1702", x: "724", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "1602", x: "724", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "1502", x: "724", y: "282", class: "sold", coords_on_click: "820,326,936,356"},
+	  	{code: "1402", x: "724", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "1302", x: "724", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "1202", x: "724", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "1102", x: "724", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "1002", x: "724", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "902", x: "724", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "802", x: "724", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "702", x: "724", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	
+	  	/* secound block */
+	  	{code: "3001", x: "209", y: "62", class: "", coords_on_click: "820,240,936,274"},
+	  	{code: "2901", x: "209", y: "234", class: "", coords_on_click: "820,286,936,317"},
+	  	{code: "2801", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	
+	  	{code: "2701", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "2601", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "2501", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "2401", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "2301", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "2201", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "2101", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "2001", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "1901", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "1801", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "1701", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "1601", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "1501", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "1401", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "1301", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "1201", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "1101", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "1001", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "901", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "801", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
+	  	{code: "701", x: "209", y: "282", class: "", coords_on_click: "820,326,936,356"},
 	  ];
 	});
 
